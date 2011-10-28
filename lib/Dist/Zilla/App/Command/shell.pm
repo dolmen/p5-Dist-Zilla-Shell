7957e19472ad332dec103de8073f2090601a5ae4
Olivier Mengué <dolmen@cpan.org>
2011-10-28 02:37:10 +0200

    Commands which are not DZ are now run as system commands

--- lib/Dist/Zilla/App/Command/shell.pm
+++ lib/Dist/Zilla/App/Command/shell.pm
@@ -10,13 +10,15 @@ sub abstract { "open a interactive shell to run other DZ commands" }
 sub execute
 {
     my $self = shift;
+
     require Term::ReadLine;
     require Text::ParseWords;
 
     my $term = Term::ReadLine->new('Dist::Zilla shell');
     my $prompt = 'DZ> ';
 
-    #my $DZA = Dist::Zilla::App->new;
+    # Get the list of DZ commands
+    my %builtins = map { ($_ => 1) } $self->app->command_names;
 
     while (1) {
 	my $line = $term->readline($prompt);
@@ -25,12 +27,13 @@ sub execute
 	next unless @ARGV;
 	last if $ARGV[0] =~ /\A(?:exit|x|quit|q)\z/;
 
-	# Unfortunately we can not reuse the same Dist::Zilla::App object
-	# (at least it does not work as expected)
-	#$DZA->execute_command($DZA->prepare_command(@ARGV));
-	# So, we use a new one for each run
-	local $@;
-	eval { Dist::Zilla::App->new->run }
+	if (exists $builtins{$ARGV[0]}) {
+	    local $@;
+	    eval { Dist::Zilla::App->new->run(@ARGV) };
+	} else {
+	    # Pass the line as-is to the shell
+	    system $line;
+	}
     }
 }
 
@@ -63,10 +66,20 @@ interactive shell is opened. You can then run any other Dist::Zilla
 command that you usually run with "dzil I<command>" (even C<shell> itself, to
 open a sub-shell, but that is useless). Type C<q|quit|exit|x> to exit the shell.
 
+Any unknown command is executed in a system shell, so you can mix DZ commands
+and system commands (ls, prove, git...).
+
 Running DZ commands from a shell brings the benefit of avoiding the huge
 startup cost due to Moose and all Dist::Zilla plugins. So the first run of
 a command under the shell may be still slow, but any successive run will be
 much faster.
+
+=head1 TRIVIA
+
+I started to seriously learn L<Dist::Zilla> at the QA Hackathon 2011 in
+Amsterdam. I immediately had the idea of this shell as DZ is really
+slow to start. Six months after, this the first DZ extension that I have
+written.
 
 =head1 SEE ALSO
 
